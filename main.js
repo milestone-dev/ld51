a5c1dc3549799a116f21dddd5591884b5cfb0de5
Milestone <games@kallepersson.se>
2022-10-01 02:11:36 +0200

    Implement prototype unit type table, add resource depot and node

--- main.js
+++ main.js
@@ -1,27 +1,47 @@
 const Type = {
-	Worker: 0,
+	Undefined: 0,
+	Harvester: 1,
 	ResourceDepot: 100,
 	ResourceNode: 500,
 }
 
 const Order = {
-	Idle: 0,
-	MoveToPoint: 1,
-	AttackToPoint: 2,
-	MoveToHarvest: 3,
-	Harvest: 4,
-	ReturnToResourceDepot: 5,
+	Undefined: 0,
+	Idle: 1,
+	MoveToPoint: 2,
+	AttackToPoint: 3,
+	MoveToHarvest: 4,
+	Harvest: 5,
+	ReturnToResourceDepot: 6,
 }
 
+
 const UNIT_SELECTOR = "x-unit";
-const UNIT_SIZE = 48;
+const UNIT_SIZE_SMALL = 24;
+const UNIT_SIZE_MEDIUM = 48;
+const UNIT_SIZE_LARGE = 64;
+const UNIT_SIZE_XLARGE = 128;
+const PLAYER_NEUTRAL = 0;
+const PLAYER_HUMAN = 1;
+
+const UnitTypeData = [];
+function AddUnit(type, name, icon, size, hp, moveSpeed, elevation) {UnitTypeData[type] = {type, name, icon, size, hp, moveSpeed, elevation}; }
+AddUnit(Type.Harvester, "Harvester", "👾", UNIT_SIZE_MEDIUM, 100, 200,1000);
+AddUnit(Type.ResourceDepot, "Base", "🛰", UNIT_SIZE_XLARGE, 100, 0, 500);
+AddUnit(Type.ResourceNode, "Crystal", "🪨", UNIT_SIZE_LARGE, 100, 0, 0);
+
+
+var mouseX, mouseY;
+
+function px(i) {return i+"px"};
 
 class UnitElement extends HTMLElement {
 
 	constructor() {
 		super();
-		this.order = Order.Idle;
-		this.type = Type.Worker;
+		this.playerID = PLAYER_NEUTRAL;
+		this.order = Order.Undefined;
+		this.type = Type.Undefined;
 		this.isMoving = false;
 		this.moveSpeed = 200;
 		this.hp = 100;
@@ -36,17 +56,36 @@ class UnitElement extends HTMLElement {
 	}
 
 	connectedCallback() {
-		this.style.width = UNIT_SIZE;
-		this.style.height = UNIT_SIZE;
 		this.style.left = 0;
 		this.style.top = 0;
-		this.style.lineHeight = UNIT_SIZE+"px";
-		this.style.fontSize = UNIT_SIZE+"px";
-
-		this.innerText = "👾";
+		
 
 		this.addEventListener("transitionstart", this.onTransitionStart);
 		this.addEventListener("transitionend", this.onTransitionEnd);
+	}
+
+	Setup(type, playerID) {
+		this.type = type;
+		this.playerID = playerID;
+		const data = UnitTypeData[this.type];
+
+		this.style.width = data.size;
+		this.style.height = data.size;
+		this.style.lineHeight = px(data.size);
+		this.style.fontSize = px(data.size);
+		this.style.zIndex = px(data.elevation);
+		this.innerText = data.icon;
+		this.hp = data.hp;
+		this.moveSpeed = data.moveSpeed;
+		// TODO add swich here to manage unit types
+		this.order = Order.Idle;
+		this.dataset.type = this.type;
+	}
+
+	Move(x, y) {
+		this.style.left = px(x);
+		this.style.top = px(y);
+		console.log("move",x,y);
 	}
 
 	get centerX() {
@@ -67,11 +106,15 @@ class UnitElement extends HTMLElement {
 
 	orderToPoint(orderX, orderY) {
 		const r = this.getBoundingClientRect();
-		const moveDuration = this.distanceToPoint(orderX,orderY) / this.moveSpeed;
+		const distance = this.distanceToPoint(orderX,orderY);
+		const moveDuration = distance / this.moveSpeed;
+		console.log(distance);
 		this.style.transitionDuration = moveDuration + "s";
-		this.style.left = orderX - r.width/2 + "px";
-		this.style.top = orderY - r.height/2 + "px";
+		this.style.left = px(orderX - r.width/2);
+		this.style.top = px(orderY - r.height/2);
 		// this.offsetHeight;
+		console.log(orderX, orderY, moveDuration);
+
 	}
 }
 
@@ -88,9 +131,11 @@ function DeselectAllUnits() {
 	document.querySelectorAll(UNIT_SELECTOR).forEach(unitElm => unitElm.classList.remove("selected"));
 }
 
-function CreateUnit(type) {
+function CreateUnit(type, playerID,x, y) {
 	const unitElm = document.createElement(UNIT_SELECTOR);
+	unitElm.Setup(type, playerID);
 	document.body.appendChild(unitElm);
+	unitElm.Move(x,y);
 }
 
 function Tick(ms) {
@@ -105,8 +150,15 @@ document.addEventListener("DOMContentLoaded", evt => {
 		evt.target.select();
 	});
 
+	document.addEventListener("mousemove", evt => {
+		mouseX = evt.pageX;
+		mouseY = evt.pageY;
+	})
+
 	document.addEventListener("keyup", evt => {
-		if (evt.key == "u") CreateUnit();
+		if (evt.key == "u") CreateUnit(Type.Harvester, PLAYER_HUMAN, mouseX, mouseY);
+		else if (evt.key == "b") CreateUnit(Type.ResourceDepot, PLAYER_HUMAN, mouseX, mouseY);
+		else if (evt.key == "r") CreateUnit(Type.ResourceNode, PLAYER_NEUTRAL, mouseX, mouseY);
 	});
 
 	document.addEventListener("contextmenu", evt => {
